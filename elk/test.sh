1520f55b72f675e6cd9d07f4e5ae0c6cfabc2869
yeliwei <a0970366492@gmail.com>
2024-03-17 00:52:42 +0800

    y

--- elk/test.sh
+++ elk/test.sh
@@ -1,2 +1,2 @@
 curl -X POST "localhost:9200/test/_doc/?pretty" -H 'Content-Type: application/json' -d' { "@timestamp": "2024-03-16T16:00:00", "message": "GET /search HTTP/1.1 200 1070000", "user": { "id": "kimchy" } } '
-curl -X POST "localhost:9200/test/_doc/?pretty" -H 'Content-Type: application/json' -d' { "@timestamp": "2024-03-16T18:00:00", "message": "GET /search HTTP/1.1 200 1070000", "user": { "id": "kimchy" } } '
+curl -X POST "localhost:9200/test/_doc/?pretty" -H 'Content-Type: application/json' -d' { "@timestamp": "2024-03-16T16:50:00", "message": "GET /search HTTP/1.1 200 1070000", "user": { "id": "kimchy" } } '
