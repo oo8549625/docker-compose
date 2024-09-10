78becedf07984db43bf482e5fb65974a96e738ba
Owen <op_owen@codingpp.com>
2024-09-10 13:23:13 +0800

    update

--- filebeat/log.sh
+++ filebeat/log.sh
@@ -1,4 +1,4 @@
-bash -c 'cat <<EOF >> log/auth1.log
+bash -c 'cat <<EOF >> log/auth.log
 Sep  2 09:16:23 ELK-test sshd[899904]: Accepted password for root from 127.0.0.1 port 62380 ssh2
 Sep  2 09:20:26 ELK-test sshd[900094]: Failed password for root from 127.0.0.1 port 62466 ssh2
 Sep  2 09:20:48 ELK-test sshd[900107]: Failed password for invalid user roota from 127.0.0.1 port 62472 ssh2
